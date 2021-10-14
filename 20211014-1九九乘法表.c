2f036cb5141aef4a7eed65ec5007e025ac054e4b
LFSW元立 <2841094049@qq.com>
2021-10-14 09:02:06 +0800

    Add files via upload

---
+++ 20211014-1九九乘法表.c
@@ -0,0 +1,23 @@
+/**********
+time:20211014
+name:九九乘法表
+By LFSW元立
+**********
+
+
+
+
+#include<stdio.h>
+#include<stdlib.h>//调用system的头文件
+main()
+{int i,j,result;
+for(i=1;i<10;i++)
+
+{for(j=1;j<10;j++)
+
+{result=i*j;
+printf("%d*%d=%-3d",i,j,result);//-3用于对齐
+}
+printf("\n");
+}
+system("pause"); } //暂停
