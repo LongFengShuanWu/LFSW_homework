d90666f6a347a029910e88522e80e8e78a07c46e
LFSW元立 <2841094049@qq.com>
2021-10-19 21:01:11 +0800

    Add files via upload

---
+++ 20211019-2作业平均分.c
@@ -0,0 +1,18 @@
+/**********
+time:20211019-2
+name:作业平均分
+By LFSW元立
+
+
+
+
+**********/
+#include<stdio.h>
+
+int main()
+{
+    int math=87,english=72,computer=93;
+    int average=(math+english+computer)/3;
+    printf("平均分为%d\n",average);
+    return 0;
+}
